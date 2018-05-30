df823c42cd7395a8a2260939f1c8ba386132621f
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2018-05-30 23:59:48 +0300

    Update AesEncryption.cs

--- encryptionClasses/AesEncryption.cs
+++ encryptionClasses/AesEncryption.cs
@@ -48,9 +48,15 @@ class AesEncryption
 		byte[] iv = RandomBytes(ivLen);
 		byte[] salt = RandomBytes(saltLen);
 		byte[][] keys = Keys(password, salt);
-		key = keys[0];
-		byte[] ciphertext = Transformer(data, key, iv, true);
+		this.key = keys[0];
 
+		byte[] ciphertext;
+		RijndaelManaged cipher = Cipher();
+		using (ICryptoTransform ict = cipher.CreateEncryptor(key, iv))
+		{
+			ciphertext = ict.TransformFinalBlock(data, 0, data.Length);
+		}
+		
 		List<byte> newData = new List<byte>();
 		newData.AddRange(iv);
 		newData.AddRange(ciphertext);
@@ -81,7 +87,7 @@ class AesEncryption
 				data = Convert.FromBase64String((Encoding.ASCII.GetString(data)));
 			int minLen = Convert.ToInt32(mode == "CBC") * (blockSize / 8);
 			if (data.Length < saltLen + ivLen + minLen + macLen)
-				throw new Exception("Not enough data.");
+				throw new Exception("Not enough data!");
 
 			List<byte> decoded = new List<byte>(data);
 			int ctSize = decoded.Count - (saltLen + ivLen + macLen);
@@ -89,13 +95,19 @@ class AesEncryption
 			byte[] iv = decoded.GetRange(saltLen, ivLen).ToArray();
 			byte[] ciphertext = decoded.GetRange(saltLen + ivLen, ctSize).ToArray();
 			byte[] hmac = decoded.GetRange(decoded.Count - macLen, macLen).ToArray();
+			
 			byte[][] keys = Keys(password, salt);
-			key = keys[0];
+			this.key = keys[0];
 			byte[] iv_ct = decoded.GetRange(saltLen, decoded.Count - (saltLen + macLen)).ToArray();
-
 			if (!Verify(iv_ct, hmac, keys[1]))
 				throw new Exception("MAC verification failed.");
-			byte[] decrypted = Transformer(ciphertext, key, iv, false);
+			
+			byte[] decrypted;
+			RijndaelManaged cipher = Cipher();
+			using (ICryptoTransform ict = cipher.CreateDecryptor(key, iv))
+			{
+				decrypted = ict.TransformFinalBlock(ciphertext, 0, ciphertext.Length);
+			}
 			return decrypted;
 		}
 		catch (Exception e)
@@ -116,34 +128,28 @@ class AesEncryption
 	/// <returns>Random bytes.</returns>
 	public byte[] RandomBytes(int size = 16)
 	{
-		byte[] iv = new byte[size];
+		byte[] rb = new byte[size];
 		using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
 		{
-			rng.GetBytes(iv);
+			rng.GetBytes(rb);
 		}
-		return iv;
+		return rb;
 	}
-
-	/// <summary>Encrypts and decrypts data.</summary>
-	/// <param name="data">The data</param>
-	/// <param name="key">The key</param>
-	/// <param name="iv">The IV</param>
-	/// <param name="encrypt">Flag</param>
-	/// <returns>Eecrypted / derypted data.</returns>
-	private byte[] Transformer(byte[] data, byte[] key, byte[] iv, bool encrypt)
+	
+	/// <summary>
+	/// Creates a RijndaelManaged object and sets some attributes.
+	/// RijndaelManaged is used because AesManaged doesn't accept unpaded blocks (CFB)
+	/// </summary>
+	private RijndaelManaged Cipher()
 	{
-		using (RijndaelManaged cipher = new RijndaelManaged())
-		{
-			cipher.Mode = modes[mode];
-			cipher.KeySize = keySize;
-			cipher.Padding = (mode == "CFB") ? PaddingMode.None : PaddingMode.PKCS7;
-			cipher.FeedbackSize = (mode == "CFB") ? 8 : blockSize;
-			cipher.BlockSize = blockSize;
-			using (ICryptoTransform ict = encrypt ? cipher.CreateEncryptor(key, iv) : cipher.CreateDecryptor(key, iv))
-			{
-				return ict.TransformFinalBlock(data, 0, data.Length);
-			}
-		}
+		RijndaelManaged cipher = new RijndaelManaged();
+		cipher.Mode = modes[mode];
+		cipher.KeySize = keySize;
+		cipher.Padding = (mode == "CFB") ? PaddingMode.None : PaddingMode.PKCS7;
+		cipher.FeedbackSize = (mode == "CFB") ? 8 : blockSize;
+		cipher.BlockSize = blockSize;
+		cipher.Key = this.key;
+		return cipher;
 	}
 
 	/// <summary>Creates a pair of keys.</summary>
@@ -179,13 +185,13 @@ class AesEncryption
 	/// <returns>True if the MACs match else false.</returns>
 	private bool Verify(byte[] data, byte[] mac, byte[] key)
 	{
+		bool match = true;
 		byte[] data_mac = Sign(data, key);
 		for (int i = 0; i < data_mac.Length; i++)
 		{
-			if (data_mac[i] != mac[i])
-				return false;
+			match = (data_mac[i] == mac[i]);
 		}
-		return true;
+		return match;
 	}
 }
 
